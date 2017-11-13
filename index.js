199590a70c1b084cb1b30da033c666a7ec3fdea3
Mike Bannister <notimpossiblemike@gmail.com>
2017-11-12 20:01:10 -0500

    Cleanup

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const micro = require('micro')
 
-// Pass control back to express if micro didn't handle the request
-const hasResponseOrNext = (res, next) => result => {
-  return result || res.headersSent ? result : next()
-}
-
-// Invoke micro app as express handler
-const microAppToExpressHandler = microApp => (req, res, next) => (
-  micro
-    .run(req, res, microApp)
-    .then(hasResponseOrNext(res, next))
+// Detect if request has been handled by micro, otherwise passes control
+// back to express.
+const invokeWithExpressIfNotHandledByMicro = (res, next) => result => (
+  // Micro handled the request if the handler returns a result (it will
+  // likely be something JSON-parse-able or a Promise instance). Micro also
+  // handled the request if any headers have been sent.
+  result || res.headersSent
+    // Micro handled it so there's nothing to do
+    ? result
+    // Pass control back to express
+    : next()
 )
 
-module.exports = microAppToExpressHandler
+// Creates an express request handler from a micro app
+const microToExpressAdapter = microHandler => (req, res, next) => (
+  micro
+    // Let micro invoke the handler
+    .run(req, res, microHandler)
+    // Pass response
+    .then(invokeWithExpressIfNotHandledByMicro(res, next))
+)
+
+module.exports = microToExpressAdapter
